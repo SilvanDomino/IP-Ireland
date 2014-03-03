5bf056b171a88f2f89ae12768c3000335f6d885c
elisabell <elisa.pijn@hva.nl>
2014-03-03 12:17:06 +0100

    camera 2 ook zoomen

---
+++ unitytest1/Assets/zoomcamera2.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class zoomcamera2 : MonoBehaviour {
+
+	Camera cam;
+	// Use this for initialization
+	void Start () {
+		cam = GameObject.Find("Camera2").camera;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetAxis("Mouse ScrollWheel") <0)
+			
+		{
+			
+			if (cam.fieldOfView<=100)
+				
+				cam.fieldOfView +=2;
+			
+			if (cam.orthographicSize<=20)
+				
+				cam.orthographicSize +=0.5f;
+			
+		}
+		
+		
+		//----------------Code for Zooming In-----------------------
+		
+		if (Input.GetAxis("Mouse ScrollWheel") > 0)
+			
+		{
+			
+			if (cam.fieldOfView>2)
+				
+				cam.fieldOfView -=2;
+			
+			if (cam.orthographicSize>=1)
+				
+				cam.orthographicSize -=0.5f;
+			
+		}
+	}
+}
