9a5237633b65c46498f2209aae10f1c1f07bd8fc
Nathan van Geffen <nathan.van.geffen@hva.nl>
2014-03-03 13:42:39 +0100

    Movement

--- unitytest1/Assets/MovementClick.cs
+++ unitytest1/Assets/MovementClick.cs
@@ -3,22 +3,30 @@ using System.Collections;
 
 public class MovementClick : MonoBehaviour {
 	Camera usedCam;
-	private Transform tr;
+	private bool isSelected;
+	private Transform trans;
 	private Vector3 destinationPosition;
 	private float destinationDistance;
 	public float moveSpeed;	
-
+	
 	// Use this for initialization
 	void Start () {
-		tr = transform;
-		destinationPosition = tr.position;
+		isSelected = false;
+		trans = transform;
+		destinationPosition = trans.position;
 		usedCam = GameObject.Find("Camera2").camera;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		destinationDistance = Vector3.Distance(destinationPosition, tr.position);
-		if(destinationDistance < 0.5f)
+		if (Input.GetMouseButtonDown (1))
+		{
+			isSelected = false;
+			Debug.Log ("Annuleer");
+		}
+		
+		destinationDistance = Vector3.Distance(destinationPosition, trans.position);
+		if(destinationDistance < 0.1f)
 		{		
 			moveSpeed = 0;
 		}
@@ -26,23 +34,31 @@ public class MovementClick : MonoBehaviour {
 		{			
 			moveSpeed = 3;
 		}
-
-		if (Input.GetMouseButtonDown (0)) 
+		
+		
+		if (Input.GetMouseButtonDown (0))
 		{
-			Plane playerPlane = new Plane(Vector3.up, transform.position);
+			Plane playerPlane = new Plane(Vector3.up,trans.position);
 			Ray ray = usedCam.ScreenPointToRay(Input.mousePosition);
 			float hitdist = 0.0f;
-
-			if (playerPlane.Raycast(ray, out hitdist))
+			
+			if(playerPlane.Raycast(ray, out hitdist))
 			{
 				Vector3 targetPoint = ray.GetPoint(hitdist);
-				destinationPosition = ray.GetPoint(hitdist);
-				Quaternion targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
-				transform.rotation = targetRotation;
+				if((targetPoint.x > trans.position.x - trans.localScale.x && targetPoint.x < trans.position.x) && (targetPoint.z > trans.position.z - trans.localScale.z && targetPoint.z < trans.position.z) )
+				{
+					Debug.Log("Poppetje geselecteerd");
+					isSelected = true;
+				}
+				else if(isSelected == true)
+				{
+					destinationPosition = ray.GetPoint(hitdist);
+					Quaternion targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
+					trans.rotation = targetRotation;
+				}
 			}
 		}
-			tr.position = Vector3.MoveTowards(tr.position, destinationPosition, moveSpeed * Time.deltaTime);
-		
+		trans.position = Vector3.MoveTowards(trans.position, destinationPosition, moveSpeed * Time.deltaTime);		
 	}
 	
 }
