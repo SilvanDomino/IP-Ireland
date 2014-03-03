a4d09840aee2d61cebfc12134117b1b6a48c3fc2
elisabell <elisa.pijn@hva.nl>
2014-03-03 16:05:23 +0100

    1 menu button

---
+++ unitytest2/Assets/menutest.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class menutest : MonoBehaviour {
+
+	bool geklikt = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseOver(){
+		if (Input.GetMouseButtonDown (0)) {
+			Debug.Log ("klikje");
+			geklikt = true;
+		}
+	}
+
+	void OnGUI(){
+		if (geklikt == true) {
+						if (GUI.Button (new Rect (1000, 30, 100, 70), "button")) {
+								Debug.Log ("geklikt op button");
+						}
+				}
+	}
+}
